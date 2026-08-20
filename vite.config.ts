e2dd11d812ed0e10324a3a91a86b0c4056da5693
Hamdy Abu El-Khair <hamdyabuelkhair@gmail.com>
2026-08-20 23:03:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
   vite: {
     base: "/top-trust/",
-    build: {
-      outDir: "dist",
-    },
   },
 });
